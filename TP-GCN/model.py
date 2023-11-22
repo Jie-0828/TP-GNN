7294a7d483cc8c2347a3c7afbdc087451c0073fa
Jie Liu <105060483+Jie-0828@users.noreply.github.com>
2023-11-22 14:05:57 +0800

    Update model.py

--- TP-GCN/model.py
+++ TP-GCN/model.py
@@ -28,8 +28,6 @@ class Model(nn.Module):
             feat_data= self.get_nodes_embedding_GRU(raw_features,dict_edge)# Get the feature of each node after aggregation:
         elif self.update_method == 'sum':
             feat_data= self.get_nodes_time_embedding(raw_features, dict_edge)
-        elif self.update_method == 'matrix':
-            feat_data = self.get_nodes_embedding_matrix(raw_features, dict_edge)
         else:
             feat_data=self.random(raw_features, dict_edge)
 
@@ -51,42 +49,6 @@ class Model(nn.Module):
             output, h_n=self.updater(unpate_feat,target_feat)
             self_feats[target] = F.normalize(h_n).squeeze(dim=0)
         return self_feats
-
-
-    def get_nodes_embedding_matrix(self, self_feats, edge_order):
-        dict_time = {}  # Record the time of the last interaction
-        time_mx = torch.zeros(len(self_feats), self.time_dim)  # Initial temporal matrix
-
-        for node in range(len(self_feats)):
-            dict_time[node] = 0  # Initialize the interaction time
-
-        dict_edge={}
-        for edge in edge_order:
-            time = int(edge[2])
-            if int(time) not in dict_edge:
-                dict_edge[int(time)] = [edge]
-            else:
-                dict_edge[int(time)].append(edge)
-
-        for time, edges in dict_edge.items():
-            a_t = torch.eye(len(self_feats))  # Define the initial adjacency matrix
-            target_nodes = []
-            for edge in edges:
-                a_t[edge[1], edge[0]] += 1
-                target_nodes.append(int(edge[1]))
-
-            a_t = torch.tensor(normalize(a_t))
-            self_feats = torch.mm(a_t, self_feats)
-
-            for item in target_nodes:
-                time_feature = self.time2vec(torch.Tensor([[time]]))
-                time_feature = F.normalize(time_feature).squeeze(dim=0)
-                time_mx[item] +=time_feature
-                dict_time[item] = time  # Update interaction time
-
-
-        feat_new = torch.cat((self_feats, time_mx), dim=1)
-        return feat_new
 
     def get_nodes_time_embedding(self, ori_feats, edge_order):
         dict_time={}
